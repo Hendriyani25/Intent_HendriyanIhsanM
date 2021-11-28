cf935d94dd2df52a6e6633d42e6460c08296b2c9
Hendriyani25 <hendriyaniii25@gmail.com>
2021-11-28 12:46:20 +0700

    Latihan Intent

---
+++ LatihanIntentSederhana_HendriyanIhsanM/app/src/main/java/com/example/latihanintentsederhana_hendriyanihsanm/IntentActivity.java
@@ -0,0 +1,14 @@
+package com.example.latihanintentsederhana_hendriyanihsanm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
+
